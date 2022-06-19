358390097f21717d15ca0a0cbbbde4e62770ce04
Jaehyeon <parkjh9370@naver.com>
2022-06-19 13:46:41 +0900

    add cat module

--- prac/src/app.controller.ts
+++ prac/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Body, Param } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
   }
 }
+
+/**
+ * 의존성 주입
+ * AppController : 소비자
+ * AppService: 제품
+ * 제품을 소비자가 사용하고 있음
+ * Provider: 공급자
+ */
--- prac/src/app.module.ts
+++ prac/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CatsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/**
+ * 의존성 주입
+ * AppController : 소비자
+ * AppService: 제품
+ * 공급자가 제공해주는 제품을 소비자가 사용하고 있음
+ * Provider: 공급자
+ */
